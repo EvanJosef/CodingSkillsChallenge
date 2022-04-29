77f326230d2efdfa057b27502c10ab2c4dca92d8
EvanJosef <evanjk@rams.colostate.edu>
2022-04-29 08:24:14 -0600

    Erroneous Statement/Column
    Added statement to append a column at the end of the dataframe stating erroneous where there are missing values. Also checked to make sure there were the right amount.

--- check.py
+++ check.py
@@ -3,12 +3,22 @@ import numpy as np
 import datetime as dt
 import pytz
 
-raw = "rawdata.xlsx"
+raw = "data/rawdata.xlsx"
 
 df = pd.read_excel(raw)
 
 # change data to a type of datetime 
 df['time'] = pd.to_datetime(df["time"], utc = True)
+
+# how many total missing values in VTWS_AVG
+missing = sum(df["VTWS_AVG"].isnull())
+
+# adding erroneous column 
+df["data qc flag VTWS_AVG"] = np.where(df["VTWS_AVG"].isnull(), "Erroneous", "")
+
+# checking how many erroneous values there are compared to missing 
+# (should be a true logical statement as there are just as many missing as Erroneous)
+missing == sum(df["data qc flag VTWS_AVG"] == "Erroneous")
 
 
 
