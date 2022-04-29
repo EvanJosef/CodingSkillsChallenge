ce3480e527464e85bc6a8815bf3ca3e890422874
EvanJosef <evanjk@rams.colostate.edu>
2022-04-29 08:18:46 -0600

    DateTime
    Changed our time column to type of datetime.

--- check.py
+++ check.py
@@ -1,4 +1,15 @@
 import pandas as pd
+import numpy as np
 import datetime as dt
-import pytz 
+import pytz
+
+raw = "rawdata.xlsx"
+
+df = pd.read_excel(raw)
+
+# change data to a type of datetime 
+df['time'] = pd.to_datetime(df["time"], utc = True)
+
+
+
 
